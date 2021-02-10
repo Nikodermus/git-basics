6971b3875de03d15d23fa3334f5cfd42057aa60c
Nicolas M. Pardo <nicolasm@nikodermus.media>
2021-02-09 20:45:05 -0500

    instrucciones ejercicio

--- test.js
+++ test.js
@@ -1,1 +1,6 @@
-console.log('Holi :)');
+console.log('Holi');
+
+// crear una carpeta
+// crear un repositorio con esa carpeta
+// agregar y modificar archivos
+// crear un commit con todos los cambios
